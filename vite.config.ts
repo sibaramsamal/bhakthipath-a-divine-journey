8ad9ce2b1f1010293b8ae522acb1ee6427667b2d
Sibaram Samal <sibaramsamal04@gmail.com>
2025-08-05 01:34:10 +0530

    github pages initial push

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/bhakthipath-a-divine-journey/', 
   server: {
     host: "::",
     port: 8080,
